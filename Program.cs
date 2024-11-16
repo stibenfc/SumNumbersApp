610c84103cdd141485709d9df999dc46178d096d
Junnior <stibenfc@hotmail.com>
2024-11-16 13:00:34 -0500

    Implementada la funcionalidad de suma de dos numeros

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main (string[] args)
+    {
+        //Solicitar al usuario el primer número
+        Console.Write("Ingresa el primer número: ");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        //Solicitar al usuario el segundo número
+        Console.Write("Ingresa el segundo número: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        //Sumar los números
+        int suma = numero1 + numero2;
+
+        //Mostrar el resultado
+        Console.WriteLine($"La suma de {numero1} y {numero2} es: {suma}");
+
+    }
+}
